e236690e941ffef7b5323c0063c8513583d54e00
kristie <kristiemconnor@gmail.com>
2019-11-11 22:48:42 -0500

    adds filters ! and directions to ingredients index

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,34 +1,45 @@
 <template>
+  <section class="module divider">
   <div class="users-ingredients">
-      <h4>{{ user.first_name }} {{ user.last_name }}'s Pantry</h4>
+    <h4>{{ user.first_name }}'s Pantry</h4>
         <h4>Ingredients on hand: {{ user.ingredients.length }}</h4>
       <br>
-        <h4>Add an ingredient</h4>
+        <p><h5>Select up to three ingredients, then click "find recipe" to search our recipe database.</h5> 
+        </p>
+      <h4>Add an ingredient</h4>
         Name: <input type="text" v-model="newIngredientName"><br>
         Expiration: <input type="date" v-model="newIngredientExpiration"><br>
-        <button v-on:click="addIngredient">Add</button>
+        <button v-on:click="addIngredient" class="btn-sm">Add</button>
         <br>
       <div class="sort">
-        <button v-on:click="setSortAttribute('name')"> Sort Alphabetically</button>
-        <button v-on:click="setSortAttribute('expiration')"> Sort by Expiration
-          <span v-if="sortAttribute === 'name' && sortAscending === 1"></span></button>
+        <h5>Search ingredients by name: <input type="text" v-model="titleFilter" list="names">
+          <datalist id="names">
+            <option v-for="ingredient in ingredients">{{ ingredient.name }}</option>
+          </datalist></h5>
+        <button v-on:click="setSortAttribute('name')" class="btn-sm"> Sort Alphabetically</button>
+        <button v-on:click="setSortAttribute('expiration')" class="btn-sm"> Sort by Expiration</button>
       </div>
-    <button v-on:click="findRecipes()">Find Recipes</button>
-    <li v-for="ingredient in orderBy(ingredients, 'expiration')">
+    <button v-on:click="findRecipes()" class="btn-sm">Find Recipes</button>
+    <li v-for="ingredient in orderBy(filterBy(ingredients, nameFilter, 'name'), sortAttribute)">
       <input type="checkbox" :id="ingredient.id" :value="ingredient.name" v-model="checkedIngredients">
       <label :for="ingredient.id">{{ ingredient.name }}</label>
        : will expire {{ relativeExpiration(ingredient.expiration) }} 
         <h4>Edit Ingredient</h4>
           <h5>Name: <input type="text" v-model="ingredient.name">
-          Expiration: <input type="date" v-model="ingredient.expiration">
-          <button v-on:click="updateIngredient(ingredient)">Edit</button>
-          <button v-on:click="destroyIngredient(ingredient)">Delete</button></h5>
+          Expiration: <input type="date" v-model="ingredient.expiration"></h5>
+          <button v-on:click="updateIngredient(ingredient)" class="btn-sm">Edit</button>
+          <button v-on:click="destroyIngredient(ingredient)" class="btn-sm">Delete</button>
             <br>
       </li>
   </div>
+</section>
 </template>
 
-<style></style>
+<style>
+/*  div {
+    justify-content: center;
+  }  */
+</style>
 
 <script>
 import axios from "axios";
@@ -45,7 +56,8 @@ export default {
       newIngredientExpiration: "",
       currentIngredient: "",
       checkedIngredients: [],
-      sortAttribute: ""
+      sortAttribute: "expiration",
+      nameFilter: ""
     };
   },
   created: function() {
@@ -85,7 +97,7 @@ export default {
         .catch(error => {
           this.errors = error.response.data.errors;
           console.log(error.response.data.errors);
-        })
+        });
     },
     destroyIngredient: function(ingredient) {
       axios.delete("/api/ingredients/" + ingredient.id).then(response => {
@@ -109,7 +121,7 @@ export default {
       return moment(expiration).endOf('day').fromNow();
     },
     setSortAttribute: function(attribute) {
-      this.sortAttribute === attribute;
+      this.sortAttribute = attribute;
     },
   }
 };
